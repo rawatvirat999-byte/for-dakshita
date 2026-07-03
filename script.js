5e8107e813864d37f030b31350ff45347c85e11e
rawatvirat999-byte <rawatvirat999@gmail.com>
2026-07-04 03:18:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const env=document.getElementById('env');
+const letter=document.getElementById('letter');
+const end=document.getElementById('end');
+env.onclick=()=>{letter.classList.remove('hidden');env.style.display='none';document.querySelector('.hint').style.display='none';window.scrollTo({top:500,behavior:'smooth'});}
+document.getElementById('endBtn').onclick=()=>{letter.classList.add('hidden');end.classList.remove('hidden');window.scrollTo({top:0,behavior:'smooth'});}
